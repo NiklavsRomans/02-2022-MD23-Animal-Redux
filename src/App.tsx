5c63ab2f7e36a65c2beda2923a6cca9f676051cf
Niklavs Romans <niklavsromans19@gmail.com>
2022-05-30 00:27:42 +0200

    main files

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/homepage/HomePage';
+import Header from './components/header/Header';
+import Page404 from './pages/page404/Page404';
 
 const App = () => (
   <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit
-        {' '}
-        <code>src/App.tsx</code>
-        {' '}
-        and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<Page404 />} />
+      </Routes>
+    </Router>
   </div>
 );
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './styles/flexboxgrid.css';
+import './styles/base.css';
+import { store } from './app/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
